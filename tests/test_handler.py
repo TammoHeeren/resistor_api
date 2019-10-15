bc1b6b8e237e38e3061955a3c1878e5e03a5b059
Tammo <tammo.heeren@gmail.com>
2019-10-15 16:43:10 -0700

    Move tests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,16 +1,70 @@
 import unittest
 import index
+import json
 
 
 class TestHandlerCase(unittest.TestCase):
+      
+    def test_e96(self):
+        event = {
+            'queryStringParameters': {
+                'value': '249k'
+            }
+        }
 
-    def test_response(self):
-        print("testing response.")
-        result = index.handler(None, None)
-        print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
+        result = index.handler(
+            event=event,
+            context=None
+        )
+
+        response = json.loads(result['body'])
+
+        self.assertEqual(response['closest']['E96']['nominal'], 249000)
+        self.assertEqual(response['closest']['E96']['colors'][0], 'red')
+        self.assertEqual(response['closest']['E96']['colors'][1], 'yellow')
+        self.assertEqual(response['closest']['E96']['colors'][2], 'white')
+        self.assertEqual(response['closest']['E96']['colors'][3], 'orange')
+        self.assertEqual(response['closest']['E96']['colors'][4], 'brown')
+
+    def test_e6(self):
+        event = {
+            'queryStringParameters': {
+                'value': '6.8k'
+            }
+        }
+    
+        response = index.handler(
+            event=event,
+            context=None
+        )
+    
+        response = json.loads(response['body'])
+    
+        self.assertEqual(response['closest']['E6']['nominal'], 6800)
+        self.assertEqual(response['closest']['E6']['colors'][0], 'blue')
+        self.assertEqual(response['closest']['E6']['colors'][1], 'grey')
+        self.assertEqual(response['closest']['E6']['colors'][2], 'red')
+
+    def test_e24(self):
+
+        event = {
+            'queryStringParameters': {
+                'value': '750k'
+            }
+        }
+    
+        response = index.handler(
+            event=event,
+            context=None
+        )
+    
+        response = json.loads(response['body'])
+    
+        self.assertEqual(response['closest']['E24']['nominal'], 750000)
+        self.assertEqual(response['closest']['E24']['colors'][0], 'violet')
+        self.assertEqual(response['closest']['E24']['colors'][1], 'green')
+        self.assertEqual(response['closest']['E24']['colors'][2], 'yellow')
+        self.assertEqual(response['closest']['E24']['colors'][3], 'gold')
 
 
 if __name__ == '__main__':
